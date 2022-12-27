841519bcd81af19df9fdcb041b66a612d6a749e8
Sanskar <samsamckp9@gmail.com>
2022-12-27 15:11:26 +0530

    testing dep

--- wealthwise/settings.py
+++ wealthwise/settings.py
@@ -80,13 +80,13 @@ WSGI_APPLICATION = 'wealthwise.wsgi.application'
 
 load_dotenv()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_USER_PASSWORD'),
-        'HOST': os.getenv('DB_HOST')
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv('DB_NAME'),
+    #     'USER': os.getenv('DB_USER'),
+    #     'PASSWORD': os.getenv('DB_USER_PASSWORD'),
+    #     'HOST': os.getenv('DB_HOST')
+    # }
 }
 
 
